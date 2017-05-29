b25050c1ea9c54b7ba1984c43ad97961157235dd
MalafeevW <malafeev-w@yandex.ru>
2017-05-29 23:17:59 +0700

    LinesCounter

---
+++ Malafeev.Nsudotnet.LinesCounter/Malafeev.Nsudotnet.LinesCounter/ScanInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Malafeev.Nsudotnet.LinesCounter
+{
+	class ScanInfo
+	{
+
+		public int rawLinesCount;
+		public int cuttedLinesCount;
+		public int usefullLineCount;
+		public string fileName;
+		public string path;
+
+
+		public ScanInfo() {
+		
+		}
+
+	}
+}
